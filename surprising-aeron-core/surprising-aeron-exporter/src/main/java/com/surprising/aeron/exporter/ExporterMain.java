8027ca75b3b0ef13f13208aebb651a26163d0b66
laizhen.li <laizhen.li@clickzetta.com>
2026-08-17 10:48:05 +0800

    fix(exporter): reconnect standalone Aeron sessions

--- surprising-aeron-core/surprising-aeron-exporter/src/main/java/com/surprising/aeron/exporter/ExporterMain.java
+++ surprising-aeron-core/surprising-aeron-exporter/src/main/java/com/surprising/aeron/exporter/ExporterMain.java
@@ -1,6 +1,8 @@
 package com.surprising.aeron.exporter;
 
-import com.surprising.aeron.client.AeronClientPool;
+import com.surprising.aeron.client.ResultUnknownException;
+import com.surprising.aeron.client.SurprisingAeronClient;
+import io.aeron.exceptions.TimeoutException;
 
 public final class ExporterMain {
 
@@ -9,28 +11,43 @@ public final class ExporterMain {
 
     public static void main(String[] args) throws Exception {
         var productLine = ExporterConfiguration.productLine();
-        try (var client = new AeronClientPool("core-exporter", productLine,
-                     ExporterConfiguration.aeronHosts(), ExporterConfiguration.aeronEgressHost(),
-                     ExporterConfiguration.aeronTimeout(), 1);
-             var sink = new KafkaCoreExportSink(ExporterConfiguration.kafkaProducerProperties())) {
+        long reconnectMillis = AdaptiveExportLoop.MIN_IDLE_MILLIS;
+        try (var sink = new KafkaCoreExportSink(ExporterConfiguration.kafkaProducerProperties())) {
             System.out.printf("Aeron exporter started productLine=%s topic=%s%n",
                     productLine, KafkaCoreExportSink.topic(productLine));
-            var exporter = new ReliableCoreExporter(productLine, client::submitPrepared, sink,
-                    ExporterConfiguration.batchSize());
-            var loop = new AdaptiveExportLoop(exporter::exportOnce, Thread::sleep,
-                    ExporterConfiguration.idleMillis());
-            long failureMillis = AdaptiveExportLoop.MIN_IDLE_MILLIS;
             while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    loop.runOnce();
-                    failureMillis = AdaptiveExportLoop.MIN_IDLE_MILLIS;
+                try (var client = SurprisingAeronClient.connect(productLine, ExporterConfiguration.aeronHosts(),
+                        ExporterConfiguration.aeronEgressHost(), ExporterConfiguration.aeronTimeout())) {
+                    var exporter = new ReliableCoreExporter(productLine, client::submit, sink,
+                            ExporterConfiguration.batchSize());
+                    var loop = new AdaptiveExportLoop(exporter::exportOnce, Thread::sleep,
+                            ExporterConfiguration.idleMillis());
+                    long failureMillis = AdaptiveExportLoop.MIN_IDLE_MILLIS;
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            loop.runOnce();
+                            failureMillis = AdaptiveExportLoop.MIN_IDLE_MILLIS;
+                        } catch (InterruptedException exception) {
+                            Thread.currentThread().interrupt();
+                        } catch (Exception exception) {
+                            System.err.printf("Aeron exporter cycle failed productLine=%s reason=%s%n",
+                                    productLine, exception.getMessage());
+                            throw exception;
+                        }
+                    }
                 } catch (InterruptedException exception) {
                     Thread.currentThread().interrupt();
-                } catch (Exception exception) {
-                    System.err.printf("Aeron exporter cycle failed productLine=%s reason=%s%n",
+                } catch (ResultUnknownException | TimeoutException exception) {
+                    System.err.printf("Aeron exporter reconnecting productLine=%s reason=%s%n",
                             productLine, exception.getMessage());
-                    Thread.sleep(failureMillis);
-                    failureMillis = AdaptiveExportLoop.nextIdleMillis(failureMillis,
+                    Thread.sleep(reconnectMillis);
+                    reconnectMillis = AdaptiveExportLoop.nextIdleMillis(reconnectMillis,
+                            AdaptiveExportLoop.MAX_IDLE_MILLIS);
+                } catch (Exception exception) {
+                    System.err.printf("Aeron exporter reconnecting productLine=%s reason=%s%n",
+                            productLine, exception.getMessage());
+                    Thread.sleep(reconnectMillis);
+                    reconnectMillis = AdaptiveExportLoop.nextIdleMillis(reconnectMillis,
                             AdaptiveExportLoop.MAX_IDLE_MILLIS);
                 }
             }
